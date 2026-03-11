e342bdbff15dc00249ce894c418b1339f9b2af4d
assaletsk-beep <assaletsk@gmail.com>
2026-03-11 23:01:44 +0100

    assets & MD_mesh updated

---
+++ src/assets/mesh_data 2.cpp
@@ -0,0 +1,6 @@
+#include <vector>
+
+#include "vertex.h"
+#include "mesh_data.h"
+
+MeshData::MeshData(const std::vector<Vertex>& _vertices, const std::vector<unsigned int>& _indice): vertices(_vertices), indices(_indice) {}
