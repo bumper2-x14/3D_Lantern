3b1539027da231cf0329d32fd587be66f25e3cb6
Alireza Sirjani <alirezasirjani.git@gmail.com>
2026-03-14 15:43:53 +0100

    math utility functions added to math/utility.h

---
+++ src/math/utility.h
@@ -0,0 +1,36 @@
+#ifndef _UTILITY_H
+#define _UTILITY_H
+
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+
+constexpr double PI = 3.14159265358979323846;
+
+inline double toRadians(double degrees) { return degrees * (PI / 180.0); }
+inline double toDegrees(double radians) { return radians * (180.0 / PI); }
+
+inline float toRadians(float degrees) { return degrees * (static_cast<float>(PI) / 180.0f); }
+inline float toDegrees(float radians) { return radians * (180.0f / static_cast<float>(PI)); }
+
+inline void randomSeed(unsigned int seed) { std::srand(seed); }
+inline void randomSeedTime() { std::srand(static_cast<unsigned int>(std::time(nullptr))); }
+
+
+inline int randomInt(int min, int max){
+    return min + std::rand() % (max - min + 1);
+}
+
+
+inline float randomFloat(float min, float max){
+    return min + (static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX)) * (max - min);
+}
+
+
+inline double randomDouble(double min, double max) {
+    return min + (static_cast<double>(std::rand()) / static_cast<double>(RAND_MAX)) * (max - min);
+}
+
+
+#endif
