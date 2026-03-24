2811def06ddc3b2e1356e347a7d9ee20d3ebb854
assaletsk-beep <assaletsk@gmail.com>
2026-03-24 09:55:49 +0100

    minor change

--- src/modeling/MD_Quad.cpp
+++ src/modeling/MD_Quad.cpp
@@ -10,11 +10,36 @@ MD_Quad::MD_Quad(float _width =1 , float _lenght = 1){
 }
 
 void MD_Quad::buildShape(){
+    // Asigning position
     Vec3f p0 (-1, 0, -1);
     Vec3f p1 (-1, 0,  1);
     Vec3f p2 ( 1, 0, -1);
     Vec3f p3 ( 1, 0,  1);
 
-    Vec3f n ( 0, 1, 1);
+    // Asigning normal value
+    Vec3f n (0, 1, 0);
 
+    // Asigning texteur
+    Vec2f uv0(0.0f, 0.0f);
+    Vec2f uv1(0.0f, 0.0f);
+    Vec2f uv2(0.0f, 0.0f);
+    Vec2f uv3(0.0f, 0.0f);
+
+    mesh.data->vertices.push_back(Vertex(p0, n, uv0));   
+    mesh.data->vertices.push_back(Vertex(p1, n, uv1));   
+    mesh.data->vertices.push_back(Vertex(p2, n, uv2));   
+    mesh.data->vertices.push_back(Vertex(p3, n, uv3));
+
+    mesh.data->indices.push_back(0); 
+    mesh.data->indices.push_back(1);
+    mesh.data->indices.push_back(2);
+
+    mesh.data->indices.push_back(2);
+    mesh.data->indices.push_back(1);
+    mesh.data->indices.push_back(3);
+
+}
+
+void MD_Quad::applyTransform(Transform* transform){
+    mesh.data
 }
