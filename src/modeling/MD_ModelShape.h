a44c40b3cb749a034b5ee21eab48d13b68d1f6d3
assaletsk-beep <assaletsk@gmail.com>
2026-04-11 18:30:03 +0200

    conflict ERR fixed
    conflict ERR fixed#

--- src/modeling/MD_ModelShape.h
+++ src/modeling/MD_ModelShape.h
@@ -6,11 +6,8 @@
 
 class MD_ModelShape: public  MD_Shape{
     public:
-        MD_ModelShape(const std::string& _path);
         void buildShape() override;
 
-    private:
-       std::string path;   
 };
 #endif
 
